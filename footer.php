0392df0973055dca0c4da670f098e94683552d9a
Hafiz Muhammad Furqan <93031914+furqanfarid786@users.noreply.github.com>
2023-07-22 00:21:20 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> 0320-1686065 </p>
+         <p> <i class="fas fa-phone"></i> 0320-1686065 </p>
+         <p> <i class="fas fa-envelope"></i> frqnfarid@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Lahore, Pakistan </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ 2023 by <span>Yuta Dota Drum</span> </p>
+
+</section>
